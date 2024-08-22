12679141b54d3edd71c6c6310b2745013fc34855
MUHAMMADABDULLAHBINSHARIF <happinesswhat31@gmail.com>
2024-08-22 13:47:08 +0500

    Update animals table, add new data, and implement transactions and queries

--- data.sql
+++ data.sql
@@ -1,6 +1,9 @@
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES 
-('Agumon', '2020-02-03', 0, TRUE, 10.23),
-('Gabumon', '2018-11-15', 2, TRUE, 8.00),
-('Pikachu', '2021-01-07', 1, FALSE, 15.04),
-('Devimon', '2017-05-12', 5, TRUE, 11.00);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES
+('Charmander', '2020-02-08', -11, FALSE, 0, 'unspecified'),
+('Plantmon', '2021-11-15', -5.7, TRUE, 2, 'unspecified'),
+('Squirtle', '1993-04-02', -12.13, FALSE, 3, 'unspecified'),
+('Angemon', '2005-06-12', -45, TRUE, 1, 'unspecified'),
+('Boarmon', '2005-06-07', 20.4, TRUE, 7, 'unspecified'),
+('Blossom', '1998-10-13', 17, TRUE, 3, 'unspecified'),
+('Ditto', '2022-05-14', 22, TRUE, 4, 'unspecified');
--- queries.sql
+++ queries.sql
@@ -1,23 +1,29 @@
--- Query 1: Find all animals whose name ends in "mon"
-SELECT * FROM animals WHERE name LIKE '%mon';
+-- Number of animals
+SELECT COUNT(*) FROM animals;
 
--- Query 2: List the names of all animals born between 2016 and 2019
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+-- Animals that have never tried to escape
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 
--- Query 3: List the names of all animals that are neutered and have fewer than 3 escape attempts
-SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+-- Average weight of animals
+SELECT AVG(weight_kg) FROM animals;
 
--- Query 4: List the date of birth of all animals named either "Agumon" or "Pikachu"
-SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+-- Neutered vs. Not Neutered Escape Attempts
+SELECT 
+    CASE 
+        WHEN neutered THEN 'Neutered' 
+        ELSE 'Not Neutered' 
+    END AS neutered_status,
+    AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+GROUP BY neutered_status;
 
--- Query 5: List the names and escape attempts of animals that weigh more than 10.5kg
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+-- Minimum and Maximum Weight by Species
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
 
--- Query 6: Find all animals that are neutered
-SELECT * FROM animals WHERE neutered = TRUE;
-
--- Query 7: Find all animals not named "Gabumon"
-SELECT * FROM animals WHERE name != 'Gabumon';
-
--- Query 8: Find all animals with a weight between 10.4kg and 17.3kg (inclusive)
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+-- Average Escape Attempts per Animal Type Born Between 1990 and 2000
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
